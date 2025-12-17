85f8509c6d1235b07ccb7f270090c4ba7add1913
Tajah Trapier <tajah.trapier@gmail.com>
2025-12-17 00:22:22 -0500

    Added units for minimum viable engine

--- Assets/Twinspec/Scripts/Runtime/Core/Units.cs
+++ Assets/Twinspec/Scripts/Runtime/Core/Units.cs
@@ -1,6 +1,8 @@
-using UnityEngine;
-
-public class Units
+// Assets/Twinspec/Scripts/Runtime/Core/Units.cs
+namespace Twinspec.Core
 {
-    
+    public static class Units
+    {
+        public const float Deg2Rad = 0.017453292519943295f;
+    }
 }
