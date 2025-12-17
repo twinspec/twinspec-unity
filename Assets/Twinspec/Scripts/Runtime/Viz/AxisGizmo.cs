bc499c92c547c206c9c7b5f17b661182c241f59e
Tajah Trapier <tajah.trapier@gmail.com>
2025-12-17 01:07:43 -0500

    Updated AxisGizmo.cs

--- Assets/Twinspec/Scripts/Runtime/Viz/AxisGizmo.cs
+++ Assets/Twinspec/Scripts/Runtime/Viz/AxisGizmo.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
-public class AxisGizmo
+public class AxisGizmo : MonoBehaviour
 {
-    
+    [Header("Optional axis arrows")]
+    public Transform xAxis;
+    public Transform yAxis;
+    public Transform zAxis;
+
+    public float length = 0.5f;
+
+    // Minimal API expected by RigController
+    public void Refresh()
+    {
+        // If you haven't assigned arrows, do nothing.
+        if (xAxis != null) { xAxis.localPosition = Vector3.zero; xAxis.localRotation = Quaternion.identity; xAxis.localScale = new Vector3(length, length, length); }
+        if (yAxis != null) { yAxis.localPosition = Vector3.zero; yAxis.localRotation = Quaternion.identity; yAxis.localScale = new Vector3(length, length, length); }
+        if (zAxis != null) { zAxis.localPosition = Vector3.zero; zAxis.localRotation = Quaternion.identity; zAxis.localScale = new Vector3(length, length, length); }
+    }
 }
